cee74a10a608bea8291d8eed4cf8775c5c9edacd
Marcos <szporerm@gmail.com>
2026-05-28 12:12:44 -0300

    feat: remove franchisee top navbar

--- src/app/(franchisee)/layout.tsx
+++ src/app/(franchisee)/layout.tsx
@@ -2,7 +2,6 @@ import { auth } from "@/lib/auth";
 
 export const dynamic = "force-dynamic";
 import { redirect } from "next/navigation";
-import { FranchiseeNavbar } from "@/components/franchisee/FranchiseeNavbar";
 
 export default async function FranchiseeLayout({
   children,
@@ -15,7 +14,6 @@ export default async function FranchiseeLayout({
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <FranchiseeNavbar user={session.user} />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-8">
         {children}
       </main>
